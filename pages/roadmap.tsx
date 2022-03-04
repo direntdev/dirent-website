b9e9e8a54fa44f1e289248c16a86c96033c7c807
Mateusz Migas <54471371+mateuszmigas@users.noreply.github.com>
2022-03-04 02:05:53 +0100

    roadmap first

--- pages/roadmap.tsx
+++ pages/roadmap.tsx
@@ -1,8 +1,38 @@
 import type { NextPage } from 'next';
 import { Layout } from '../components/layout';
 
+type RoadmapEntry = {
+  icon: 'feature';
+  title: string;
+  description: string;
+};
+
+const roadmap: RoadmapEntry[] = [
+  {
+    icon: 'feature',
+    title: 'Quick launch',
+    description: 'Feature description 1',
+  },
+  { icon: 'feature', title: 'Feature 2', description: 'Feature description 2' },
+  { icon: 'feature', title: 'Feature 3', description: 'Feature description 3' },
+];
+
+const RoadmapEntry = (props: { entry: RoadmapEntry }) => {
+  return <div>{props.entry.title}</div>;
+};
+
 const RoadmapPage: NextPage = () => {
-  return <Layout>Roadmap</Layout>;
+  return (
+    <Layout>
+      <div>
+        {roadmap.map((entry, index) => (
+          <div key={index}>
+            <RoadmapEntry entry={entry}></RoadmapEntry>
+          </div>
+        ))}
+      </div>
+    </Layout>
+  );
 };
 
 export default RoadmapPage;
