8cdec04a79437003756dc7b589e5d9e2f5e38c9c
Mateusz Migas <54471371+mateuszmigas@users.noreply.github.com>
2022-03-04 02:00:11 +0100

    navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,31 +4,37 @@ import Image from 'next/image';
 export const Navbar = () => {
   return (
     <>
-      <nav className="flex items-center flex-wrap bg-blue-500 p-3 gap-2">
+      <nav className="flex items-center justify-between flex-wrap bg-gray-900 p-3 text-white">
         <Link href="/">
-          <a className="">Home</a>
-        </Link>
-        <Link href="/about">
-          <a className="">about</a>
-        </Link>
-        <Link href="https://mateusz-migas.gitbook.io/dirent-docs/">
-          <a target="_blank" className="">
-            Docs
+          <a className="flex items-center gap-2 font-mono">
+            <Image alt="" src={'/logo.png'} width={24} height={24} />
+            DIRENT
           </a>
         </Link>
-        <Link href="/roadmap">
-          <a className="">Roadmap</a>
-        </Link>
-        <Link href="https://github.com/direntdev">
-          <a target="_blank" className="w-6 h-6">
-            <Image
-              alt="/github.png"
-              src={'/github.png'}
-              width={24}
-              height={24}
-            />
-          </a>
-        </Link>
+        <div className="flex items-center gap-2">
+          <Link href="/about">
+            <a className="">About</a>
+          </Link>
+          <Link href="https://mateusz-migas.gitbook.io/dirent-docs/">
+            <a target="_blank" className="">
+              Docs
+            </a>
+          </Link>
+          <Link href="/roadmap">
+            <a className="">Roadmap</a>
+          </Link>
+          <Link href="https://github.com/direntdev">
+            <a target="_blank" className="w-6 h-6 invert">
+              <Image
+                alt=""
+                src={'/github.png'}
+                className={'text-white'}
+                width={24}
+                height={24}
+              />
+            </a>
+          </Link>
+        </div>
       </nav>
     </>
   );
