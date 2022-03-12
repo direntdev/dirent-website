4695af0bbe96e4b66b5bc8edba61a6f71e003176
Mateusz Migas <54471371+mateuszmigas@users.noreply.github.com>
2022-03-12 13:00:50 +0100

    navbar fixed

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,74 +1,8 @@
-import Link from 'next/link';
 import Image from 'next/image';
-
-// export const Navbar = () => {
-//   return (
-//     <>
-//       <nav className="h-16 inset-x-0 flex items-center justify-between flex-wrap bg-skin-background1 p-3 text-white">
-//         <Link href="/">
-//           <a className="flex items-center gap-2 font-mono text-2xl">
-//             <Image alt="" src={'/logo.png'} width={24} height={24} />
-//             DIRENT
-//           </a>
-//         </Link>
-//         <div className="flex items-center gap-4">
-//           <Link href="/about-us">
-//             <a className="">About</a>
-//           </Link>
-//           <Link href="https://docs.dirent.dev/">
-//             <a target="_blank" className="">
-//               Docs
-//             </a>
-//           </Link>
-//           <Link href="/roadmap">
-//             <a className="">Roadmap</a>
-//           </Link>
-//           <Link href="/plugins">
-//             <a className="">Plugins</a>
-//           </Link>
-//           <Link href="https://github.com/direntdev">
-//             <a target="_blank" className="w-6 h-6 invert">
-//               <Image
-//                 alt=""
-//                 src={'/github.png'}
-//                 className={'text-white'}
-//                 width={24}
-//                 height={24}
-//               />
-//             </a>
-//           </Link>
-//         </div>
-//       </nav>
-//     </>
-//   );
-// };
-
-// background-color: #4158D0;
-// background-image: linear-gradient(43deg, #4158D0 0%, #C850C0 48%, #FFCC70 100%);
-
-/* This example requires Tailwind CSS v2.0+ */
-import { Fragment } from 'react';
+import Link from 'next/link';
 import { Disclosure, Menu, Transition } from '@headlessui/react';
-import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline';
+import { MenuIcon, XIcon } from '@heroicons/react/outline';
 import { routes } from '../utils/routes';
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ');
-}
-
-const NavbarLink = (props: { item: { name: string; href: string } }) => {
-  const {
-    item: { name, href },
-  } = props;
-  return (
-    <a
-      href={href}
-      className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-lg font-medium"
-    >
-      {name}
-    </a>
-  );
-};
 
 const navbarButtons = [
   routes.roadmap,
@@ -86,21 +20,30 @@ export const Navbar = () => {
             <div className="flex items-center justify-between h-16">
               {/* Logo */}
               <div className="flex-shrink-0">
-                <img
-                  className="block lg:hidden h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                  alt="Workflow"
-                />
-                <img
-                  className="hidden lg:block h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
-                  alt="Workflow"
-                />
+                <Link href="/">
+                  <a className="flex items-center gap-2 font-mono text-2xl">
+                    <Image
+                      alt="Dirent"
+                      src={'/logo.png'}
+                      width={24}
+                      height={24}
+                    />
+                    <span className="text-white font-bold font-mono uppercase">
+                      dirent
+                    </span>
+                  </a>
+                </Link>
               </div>
               {/* Button */}
               <div className="hidden sm:block sm:ml-6 space-x-4">
                 {navbarButtons.map(item => (
-                  <NavbarLink key={item.name} item={item}></NavbarLink>
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-lg font-medium"
+                  >
+                    {item.name}
+                  </a>
                 ))}
               </div>
               <div className="-mr-2 flex sm:hidden">
@@ -119,84 +62,16 @@ export const Navbar = () => {
 
           <Disclosure.Panel className="sm:hidden">
             <div className="px-2 pt-2 pb-3 space-y-1">
-              {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
-              <Disclosure.Button
-                as="a"
-                href="#"
-                className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
-              >
-                Dashboard
-              </Disclosure.Button>
-              <Disclosure.Button
-                as="a"
-                href="#"
-                className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-              >
-                Team
-              </Disclosure.Button>
-              <Disclosure.Button
-                as="a"
-                href="#"
-                className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-              >
-                Projects
-              </Disclosure.Button>
-              <Disclosure.Button
-                as="a"
-                href="#"
-                className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-              >
-                Calendar
-              </Disclosure.Button>
-            </div>
-            <div className="pt-4 pb-3 border-t border-gray-700">
-              <div className="flex items-center px-5">
-                <div className="flex-shrink-0">
-                  <img
-                    className="h-10 w-10 rounded-full"
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    alt=""
-                  />
-                </div>
-                <div className="ml-3">
-                  <div className="text-base font-medium text-white">
-                    Tom Cook
-                  </div>
-                  <div className="text-sm font-medium text-gray-400">
-                    tom@example.com
-                  </div>
-                </div>
-                <button
-                  type="button"
-                  className="ml-auto flex-shrink-0 bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                >
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
-              </div>
-              <div className="mt-3 px-2 space-y-1">
+              {navbarButtons.map(item => (
                 <Disclosure.Button
+                  key={item.name}
                   as="a"
-                  href="#"
-                  className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700"
+                  href={item.href}
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
                 >
-                  Your Profile
+                  {item.name}
                 </Disclosure.Button>
-                <Disclosure.Button
-                  as="a"
-                  href="#"
-                  className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700"
-                >
-                  Settings
-                </Disclosure.Button>
-                <Disclosure.Button
-                  as="a"
-                  href="#"
-                  className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700"
-                >
-                  Sign out
-                </Disclosure.Button>
-              </div>
+              ))}
             </div>
           </Disclosure.Panel>
         </>
